3f8f54a65ab5917cffd8d1093e5fe6b8ef58825e
mrinalraj2809 <mrinalraj2809@gmail.com>
2019-07-19 16:31:17 +0530

    Added main Writing Activity TODO :Improve text input layout

--- The CarpeDiem/app/src/main/java/com/example/thecarpediem/WritingActivity.java
+++ The CarpeDiem/app/src/main/java/com/example/thecarpediem/WritingActivity.java
@@ -1,7 +1,12 @@
 package com.example.thecarpediem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class WritingActivity extends AppCompatActivity {
 
@@ -11,3 +16,6 @@ public class WritingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_writing);
     }
 }
+
+
+
