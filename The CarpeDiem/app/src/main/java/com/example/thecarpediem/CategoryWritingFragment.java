d2d264331b6569c8ed643f964df0f03e9baf7335
mrinalraj2809 <mrinalraj2809@gmail.com>
2019-07-24 00:10:45 +0530

    Added Double Tap tfor background and viewpager setup

---
+++ The CarpeDiem/app/src/main/java/com/example/thecarpediem/CategoryWritingFragment.java
@@ -0,0 +1,29 @@
+package com.example.thecarpediem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryWritingFragment extends Fragment {
+
+
+    public CategoryWritingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category_writing, container, false);
+    }
+
+}
