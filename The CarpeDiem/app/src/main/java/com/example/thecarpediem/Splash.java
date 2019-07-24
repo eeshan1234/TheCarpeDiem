8346a12d077c8735c1cc72446f933fae8417a295
sarthaksarm <sarmsarthak@gmail.com>
2019-07-24 11:28:08 +0530

    Added Splash

--- The CarpeDiem/app/src/main/java/com/example/thecarpediem/Splash.java
+++ The CarpeDiem/app/src/main/java/com/example/thecarpediem/Splash.java
@@ -1,13 +1,54 @@
 package com.example.thecarpediem;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        configSplash.setBackgroundColor(R.color.white); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.splashimg); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn);
+
+        //Customize Path
+        // configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+
+        //Customize Title
+        configSplash.setTitleSplash("The CarpeDiem"); //change your app name here
+        configSplash.setTitleTextColor(R.color.splashtxt);
+        configSplash.setTitleTextSize(19f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FadeInLeft);
+        //configSplash.setTitleFont("font/pacifico.ttf");
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    public void animationsFinished() {
+
+        Intent intent=new Intent(Splash.this,MainActivity.class);
+        startActivity(intent);
+
     }
+
+
+
 }
