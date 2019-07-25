fc47b5ea24975f699e168fcebed4eb95f56407ce
mrinalraj2809 <mrinalraj2809@gmail.com>
2019-07-26 03:31:54 +0530

    Worked on Writing Backend

--- The CarpeDiem/app/src/main/java/com/example/thecarpediem/CongratulationWritingFragment.java
+++ The CarpeDiem/app/src/main/java/com/example/thecarpediem/CongratulationWritingFragment.java
@@ -1,11 +1,15 @@
 package com.example.thecarpediem;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 
 /**
@@ -17,13 +21,26 @@ public class CongratulationWritingFragment extends Fragment {
     public CongratulationWritingFragment() {
         // Required empty public constructor
     }
+    nl.dionsegijn.konfetti.KonfettiView konfettiView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_congratulation_writing, container, false);
+        View view = inflater.inflate(R.layout.fragment_congratulation_writing, container, false);
+        konfettiView = view.findViewById(R.id.viewKonfetti);
+        konfettiView.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12,5))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 5000L);
+        return view;
     }
 
 }
