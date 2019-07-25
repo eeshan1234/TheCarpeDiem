19b30d9fc09938b2ba857a7fb8ef54a152d69a95
mrinalraj2809 <mrinalraj2809@gmail.com>
2019-07-26 03:30:28 +0530

    Integrated Konfette feature

---
+++ The CarpeDiem/app/src/main/java/com/example/thecarpediem/CongratulationWritingFragment.java
@@ -0,0 +1,29 @@
+package com.example.thecarpediem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CongratulationWritingFragment extends Fragment {
+
+
+    public CongratulationWritingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_congratulation_writing, container, false);
+    }
+
+}
